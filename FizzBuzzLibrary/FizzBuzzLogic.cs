4512080b4d471c6aefa83482e0cc94e30459b3eb
linhng2210 <k.linh2210@gmail.com>
2024-04-05 11:18:47 -0700

    First commit

---
+++ FizzBuzzLibrary/FizzBuzzLogic.cs
@@ -0,0 +1,43 @@
+﻿namespace FizzBuzzLibrary;
+
+public class FizzBuzzLogic
+{
+    public string? GetOutput(int? number)
+    {
+        if (number == null)
+        {
+            throw new Exception("Number cannot be null");
+        }
+        if (number < 0)
+        {
+            // throw new ArgumentOutOfRangeException();
+            throw new Exception("Number must be in range 1-100");
+        }
+        if (number > 100)
+        {
+            throw new ArgumentOutOfRangeException("Number must be in range 1-100");
+        }
+        string output = string.Empty;
+        for (int i = 1; i <= number; i++)
+        {
+            if (i % 3 == 0 && i % 5 == 0)
+            {
+                output += "FizzBuzz ";
+            }
+            else if (i % 3 == 0)
+            {
+                output += "Fizz ";
+            }
+            else if (i % 5 == 0)
+            {
+                output += "Buzz ";
+            }
+            else
+            {
+                output += $"{i} ";
+            }
+            
+        }
+        return output;
+    }
+}
---
+++ FizzBuzzTests/UnitTests.cs
@@ -0,0 +1,69 @@
+namespace FizzBuzzTests;
+using FizzBuzzLibrary;
+
+public class UnitTests
+{
+    [Fact]
+    public void Given1Output1()
+    {
+        FizzBuzzLogic fb = new FizzBuzzLogic();
+        string? actual = fb.GetOutput(1);
+        string expected = "1 ";
+        Assert.Equal(expected, actual);
+    }
+
+    [Fact]
+    public void Given3Output12Fizz()
+    {
+        FizzBuzzLogic fb = new FizzBuzzLogic();
+        string? actual = fb.GetOutput(3);
+        string expected = "1 2 Fizz ";
+        Assert.Equal(expected, actual);
+    }
+
+    [Fact]
+    public void Given5Output12Fizz4Buzz() {
+        FizzBuzzLogic fb = new FizzBuzzLogic();
+        string? actual = fb.GetOutput(5);
+        string expected = "1 2 Fizz 4 Buzz ";
+        Assert.Equal(expected, actual);
+    }
+
+    [Fact]
+    public void Given15Output12Fizz__FizzBuzz() {
+        FizzBuzzLogic fb = new FizzBuzzLogic();
+        string? actual = fb.GetOutput(15);
+        string expected = "1 2 Fizz 4 Buzz Fizz 7 8 Fizz Buzz 11 Fizz 13 14 FizzBuzz ";
+        Assert.Equal(expected, actual);
+    }
+
+    [Fact]
+    public void GivenNegativeOutputException() {
+        FizzBuzzLogic fb = new FizzBuzzLogic();
+        // Assert.Throws<ArgumentOutOfRangeException>(() => fb.GetOutput(0));
+        Assert.Throws<Exception>(() => fb.GetOutput(-1));
+    }
+
+    [Fact]
+    public void GivenGreaterThan100OutputException() {
+        FizzBuzzLogic fb = new FizzBuzzLogic();
+        Assert.Throws<ArgumentOutOfRangeException>(() => fb.GetOutput(101));
+    }
+
+    [Fact]
+    public void GivenNullOutputException() {
+        FizzBuzzLogic fb = new FizzBuzzLogic();
+        Assert.Throws<Exception>(() => fb.GetOutput(null));
+    }
+
+    [Fact]
+    public void Given0OutputBlank() {
+        FizzBuzzLogic fb = new FizzBuzzLogic();
+        string? actual = fb.GetOutput(0);
+        // string expected = "";
+        string? expected = null;
+        Assert.Equal(expected, actual);
+    }
+
+
+}
